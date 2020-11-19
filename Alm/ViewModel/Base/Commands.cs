dde081b9acf7ab5600bb370f71e91511f8008e6f
EmilyEdna <847432003@qq.com>
2020-11-19 18:22:31 +0800

    select

---
+++ Alm/ViewModel/Base/Commands.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Alm.ViewModel.Base
+{
+    public class Commands<T> : ICommand
+    {
+        private readonly Action<T> _Execute;
+        private readonly Func<bool> _CanExecute;
+
+        public Commands(Action<T> Execute, Func<bool> CanExecute)
+        {
+            _Execute = Execute;
+            _CanExecute = CanExecute;
+        }
+
+        public event EventHandler CanExecuteChanged {
+            add
+            {
+                if (_CanExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (_CanExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _CanExecute == null ? true : _CanExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _Execute((T)parameter);
+        }
+    }
+}
---
+++ Alm/ViewModel/SileMenuViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Alm.ViewModel.Base;
+using HandyControl.Controls;
+using HandyControl.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alm.ViewModel
+{
+    public class SileMenuViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// 全局
+        /// </summary>
+        public Commands<FunctionEventArgs<object>> SelectCmd => new Commands<FunctionEventArgs<object>>((obj) =>
+        {
+            Growl.Info((obj.Info as SideMenuItem)?.Header.ToString());
+        }, null);
+
+        /// <summary>
+        /// 局部
+        /// </summary>
+        public Commands<string> SelectItem => new Commands<string>((obj) => 
+        {
+        
+        }, null);
+    }
+}
