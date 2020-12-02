334bd5eb4d0de58061637402cf9af36d05b7a40e
EmilyEdna <847432003@qq.com>
2020-12-02 14:07:38 +0800

    1

--- Alm/ViewModel/AuthorPageViewModel.cs
+++ Alm/ViewModel/AuthorPageViewModel.cs
@@ -2,7 +2,9 @@
 using Alm.ViewModel.Base;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using XExten.HttpFactory;
 
 namespace Alm.ViewModel
 {
@@ -11,12 +13,18 @@ namespace Alm.ViewModel
         public AuthorPageViewModel()
         {
             IocManager.SetCache(nameof(AuthorPageViewModel), this);
+            InitSupport();
         }
         private string _Support;
         public string Support
         {
             get { return _Support; }
             set { _Support = value; OnPropertyChanged("Support"); }
+        }
+
+        private void InitSupport() {
+            WebClient client = new WebClient();
+            var xx = client.DownloadString("https://raw.githubusercontent.com/EmilyEdna/Alm/master/SupportList.json");
         }
     }
 }
