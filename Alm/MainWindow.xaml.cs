61b078461ee22e59aecb6a5845170da7b6d90504
EmilyEdna <847432003@qq.com>
2021-01-09 13:48:10 +0800

    添加保护码

--- Alm/MainWindow.xaml.cs
+++ Alm/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using AlmCore.Scrapy;
 using HandyControl.Controls;
+using System;
 using System.Threading.Tasks;
 using System.Timers;
 using XExten.XPlus;
@@ -13,18 +14,30 @@ namespace Alm
     {
         public MainWindow()
         {
-            InitializeComponent();
-            Timer timer = new Timer
+            StarBox(() =>
             {
-                Interval = 60000,
-                AutoReset = false,
-                Enabled = false,
-            };
-            timer.Elapsed += (s, e) =>
-            {
-                Task.Factory.StartNew(() => XPlusEx.XTry(() => Konachan.InitTags(), ex => Growl.Info("网络连接失败!")));
-            };
-            timer.Start();
+                InitializeComponent();
+                Timer timer = new Timer
+                {
+                    Interval = 60000,
+                    AutoReset = false,
+                    Enabled = false,
+                };
+                timer.Elapsed += (s, e) =>
+                {
+                    Task.Factory.StartNew(() => XPlusEx.XTry(() => Konachan.InitTags(), ex => Growl.Info("网络连接失败!")));
+                };
+                timer.Start();
+            });
+        }
+
+        private void StarBox(Action action)
+        {
+            PINWindow box = new PINWindow();
+            if (box.ShowDialog() == true) {
+                action.Invoke();
+                box.Close();
+            }
         }
     }
 }
---
+++ Alm/PINWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using XExten.XCore;
+
+namespace Alm
+{
+    /// <summary>
+    /// PINWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class PINWindow
+    {
+        public PINWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Pwd.Password.IsNullOrEmpty())
+            {
+                ErrorInfo.Text = "解锁码错误!";
+                return;
+            }
+            var pin = DateTime.Now.ToString("yyyyMMdd");
+            if (Pwd.Password.Equals(pin))
+            {
+                DialogResult = true;
+            }
+            else
+            {
+                ErrorInfo.Text = "解锁码错误!";
+                return;
+            }
+        }
+    }
+}
